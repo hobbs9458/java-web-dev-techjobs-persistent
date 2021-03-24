1f1d6760dff6d9a20b673d0a3d51b6cac951fd5e
Logan Hobbs <hobbs9458@gmail.com>
2021-03-23 21:08:08 -0500

    Finished Final Assignment

--- queries.sql
+++ queries.sql
@@ -1,7 +1,13 @@
 ## Part 1: Test it with SQL
-
+DESCRIBE job;
 ## Part 2: Test it with SQL
-
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 ## Part 3: Test it with SQL
-
+DROP TABLE job;
 ## Part 4: Test it with SQL
+SELECT skill.name, description
+FROM skill, job_skills
+WHERE skill.id = job_skills.skill_id AND job_id IS NOT NULL
+ORDER By skill.name;
